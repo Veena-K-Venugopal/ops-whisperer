7e4091c217eeacdbe51235b28d0658771996f167
Veena <vkvenu10@gmail.com>
2026-03-25 01:07:33 -0500

    feat: add load_inventory function

--- analysis.py
+++ analysis.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv('inventory.csv')
-print(df)
+def load_inventory(filename):
+    df = pd.read_csv(filename)
+    return df
+
+if __name__ == "__main__":
+    print(load_inventory('inventory.csv'))

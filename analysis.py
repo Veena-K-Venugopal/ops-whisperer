8deb08ad1bc79e5998bc8ab3303355c7fd93e81b
Veena <vkvenu10@gmail.com>
2026-03-25 01:17:27 -0500

    feat: add get_low_stock function

--- analysis.py
+++ analysis.py
@@ -4,5 +4,10 @@ def load_inventory(filename):
     df = pd.read_csv(filename)
     return df
 
+def get_low_stock(df):
+    filtered_data = df[df['quantity'] <= 10]
+    return filtered_data
+
 if __name__ == "__main__":
-    print(load_inventory('inventory.csv'))
+    data = load_inventory('inventory.csv')
+    print(get_low_stock(data))

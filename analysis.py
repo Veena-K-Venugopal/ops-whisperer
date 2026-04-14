7fe92fb07e2790ced8e6c0b92057f051fb8922d9
Veena <vkvenu10@gmail.com>
2026-04-14 11:40:16 -0500

    feat: add category summary metric, round currency values

--- analysis.py
+++ analysis.py
@@ -9,11 +9,19 @@ def get_low_stock(df):
     return filtered_data
 
 def get_inventory_value(df):
-    total_value = (df['quantity'] * df['unit_price']).sum()
+    total_value = (df['quantity'] * df['unit_price']).sum().round(2)
     return total_value
+
+def get_category_summary(df):
+    new_df = df.copy()
+    new_df['total_value'] = (new_df['quantity'] * new_df['unit_price'])
+    grouped = new_df.groupby('category')
+    return grouped.agg({'quantity': 'sum','total_value':'sum'}).round(2)
+
 
 if __name__ == "__main__":
     data = load_inventory('inventory.csv')
     low_stock = get_low_stock(data)
     total_inventory_value = get_inventory_value(data)
-    print(total_inventory_value)
+    category_summary = get_category_summary(data)
+    print(category_summary)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi import UploadFile
 import io, pandas as pd
-from analysis import load_inventory, get_low_stock, get_inventory_value
+from analysis import load_inventory, get_low_stock, get_inventory_value, get_category_summary
 
 app = FastAPI()
 
@@ -15,4 +15,5 @@ async def upload_file(file: UploadFile):
     df = pd.read_csv(io.BytesIO(contents))
     return {"low_stock": get_low_stock(df).to_dict(orient='records'),
             "inventory_value": get_inventory_value(df),
+            "category_summary":get_category_summary(df).reset_index().to_dict(orient='records'),
             }
